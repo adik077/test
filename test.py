f117f52d457a702bb2190cce5455ecad1b7fe9a9
Adrian <aginalski112@gmail.com>
2022-04-18 14:28:24 +0200

    first version

---
+++ test.py
@@ -0,0 +1,21 @@
+class Figury:
+    def __init__(self,a,h=0):
+        self.a = a
+        self.h = h
+
+    def pole_pow(self,a,h):
+        return self
+
+class Kwadrat(Figury):
+    def pole_pow(self):
+        return self.a*self.a
+
+class Trojkat(Figury):
+    def pole_pow(self):
+        return self.a*self.h/2
+
+kwadrat = Kwadrat(5)
+trojkat = Trojkat(2,5)
+
+print(kwadrat.pole_pow())
+print(trojkat.pole_pow())
